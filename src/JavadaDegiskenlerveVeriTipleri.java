562d36319f9a6519c0802defea828017253affdf
Kübra Dönmez <kubradonmezz@outlook.com.tr>
2025-05-24 17:35:41 +0300

    [JA-2] Java'da Değişkenler ve Veri Tipleri görevi tamamlandı.

---
+++ src/JavadaDegiskenlerveVeriTipleri.java
@@ -0,0 +1,21 @@
+public class JavadaDegiskenlerveVeriTipleri {public static void main(String[] args)
+{
+
+    int yas = 31;
+    double boy = 1.65;
+    float agirlik = 58.3f;
+    boolean ogrenciMi = true;
+    char cinsiyet = 'K';
+    String isim = "Kübra";
+    final double PI = 3.14159;
+
+    System.out.println("İsim: " + isim);
+    System.out.println("Yaş: " + yas);
+    System.out.println("Boy: " + boy);
+    System.out.println("Ağırlık: " + agirlik);
+    System.out.println("Öğrenci mi? " + ogrenciMi);
+    System.out.println("Cinsiyet: " + cinsiyet);
+    System.out.println("Pi sayısı (sabit): " + PI);
+}
+
+}
